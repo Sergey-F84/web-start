9ffec7bb3afa746c708be36e5cd2d821f4244da1
Sergey-F84 <legal77@bk.ru>
2021-07-29 00:22:31 +0900

    create: modal window

--- js/main.js
+++ js/main.js
@@ -1,25 +1,29 @@
-var hotelslider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-});
-var reviewsslider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-});
+$(document).ready(function() {
+  var hotelslider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+  });
+  var reviewsslider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+  });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log('click on');
-  document.querySelector(".navbar-navigate")
-  .classList.toggle("navbar-navigate--visible");
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log('click on');
+    document.querySelector(".navbar-navigate")
+    .classList.toggle("navbar-navigate--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]")
 });
